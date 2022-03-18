0369305b567d016afa583641161722c034f01b4c
Prestondellis <98236613+Prestondellis@users.noreply.github.com>
2022-03-17 21:47:19 -0500

    made working cash register
    will add finishing touches and comments with explanation later

--- cashregister.js
+++ cashregister.js
@@ -1,5 +1,11 @@
 function checkCashRegister(price, cash, cid) {
-  var cashReg = Object.values(cid).reverse()
+  var cashReg = []
+  for (let i = 0; i < cid.length; i++){
+    let num = (cid[i][1] * 100)
+    let roundedNum = parseFloat(num).toPrecision(5)
+    cashReg.unshift(roundedNum)
+  }
+  console.log(cashReg)
   var emptyReg = [ 
     [ 'ONE HUNDRED', 0 ],
     [ 'TWENTY', 0 ],
@@ -11,22 +17,45 @@ function checkCashRegister(price, cash, cid) {
     [ 'NICKEL', 0 ],
     [ 'PENNY', 0 ]]
   var currency = [ 
-    [ 'ONE HUNDRED', 100 ],
-    [ 'TWENTY', 20 ],
-    [ 'TEN', 10 ],
-    [ 'FIVE', 5 ],
-    [ 'ONE', 1 ],
-    [ 'QUARTER', 0.25 ],
-    [ 'DIME', 0.1 ],
-    [ 'NICKEL', 0.05 ],
-    [ 'PENNY', 0.01 ]]
+    [ 'ONE HUNDRED', 10000 ],
+    [ 'TWENTY', 2000 ],
+    [ 'TEN', 1000 ],
+    [ 'FIVE', 500 ],
+    [ 'ONE', 100 ],
+    [ 'QUARTER', 25 ],
+    [ 'DIME', 10 ],
+    [ 'NICKEL', 5 ],
+    [ 'PENNY', 1 ]]
+  //console.log(cashReg)
+  let change = (cash * 100) - (price * 100);
+  //console.log(change)
+  let changeCount = 0
+  //console.log(change - changeCount)
+  for (let i = 0; i < cashReg.length; i++){
+    changeCount += emptyReg[i][1] 
+  }  
+  for (let i = 0; i < cashReg.length; i++){
+    if (changeCount < change && currency[i][1] <= change - changeCount && cashReg[i] > 0 && i >= 0){
+      changeCount += currency[i][1]
+      cashReg[i] -= currency[i][1]
+      emptyReg[i][1] += currency[i][1]
+      console.log(currency[i][1])
+      if (cashReg[i] !== 0 && currency[i][1] <= change - changeCount){
+        i -= i
+      }
+    }
+  }
   console.log(cashReg)
-  emptyReg[0][0] += 100
-  let change = cash - price;
-  let status = '' 
+  console.log(changeCount)
+  console.log(change - changeCount)
+  console.log(emptyReg)
+  
+  let status = ''; 
   if (change < 0){
     status += 'INSUFFICENT_FUNDS'
-  } else if (change > )
+  } else if (change > 0){
+    status += 'OPEN'
+  }
   return 'status: "' + status + '"';
 }
 
